6bd160454eee067a50fe9ee08cd165bed0eddc06
Marley Amged <mollyamged89@gmail.com>
2025-10-17 18:42:24 +0300

    modify database

--- create_database.py
+++ create_database.py
@@ -5,6 +5,9 @@ def create_database():
     conn = sqlite3.connect('school.db')
     cursor = conn.cursor()
 
+    # Enable foreign key support
+    cursor.execute("PRAGMA foreign_keys = ON;")
+
     # Create students table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS students (
@@ -12,19 +15,42 @@ def create_database():
             name TEXT NOT NULL,
             age INTEGER NOT NULL,
             grade INTEGER NOT NULL,
-            class CHAR NOT NULL
+            class TEXT NOT NULL,
+            num_absences INTEGER DEFAULT 0
         )
     ''')
 
-    # Create courses table with a foreign key referencing students
+    # Create courses table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS courses (
+            cid INTEGER PRIMARY KEY AUTOINCREMENT,
+            course_name TEXT NOT NULL,
+            grade INTEGER NOT NULL,
+            term INTEGER CHECK(term IN (1,2))
+        )
+    ''')
+
+    # Create grades table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS grades (
+            student_ssn TEXT,
+            course_id INTEGER,
+            scores INTEGER,
+            PRIMARY KEY (student_ssn, course_id),
+            FOREIGN KEY (student_ssn) REFERENCES students (ssn),
+            FOREIGN KEY (course_id) REFERENCES courses (cid)
+        )
+    ''')
+
+    # Create attendance table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS attendance (
             student_ssn TEXT,
             date TEXT,
             before_break_attendance BOOLEAN DEFAULT NULL,
             after_break_attendance BOOLEAN DEFAULT NULL,
-            FOREIGN KEY (student_ssn) REFERENCES students (ssn),
-            PRIMARY KEY (student_ssn, date)
+            PRIMARY KEY (student_ssn, date),
+            FOREIGN KEY (student_ssn) REFERENCES students (ssn)
         )
     ''')
 
